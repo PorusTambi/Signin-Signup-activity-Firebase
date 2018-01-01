5df12f0b01fa874404ad288a7aca58809cf7f968
Porus Tambi <porus2197@gmail.com>
2018-01-01 21:20:05 +0530

    Add files via upload

---
+++ profile_activity.java
@@ -0,0 +1,58 @@
+package com.example.porus.treasurehunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class profile_activity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
+    private EditText firstname;
+    private EditText lastname;
+//    private Firebase f;
+  private  Button getdetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_activity);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firstname = (EditText) findViewById(R.id.firstNameText);
+        lastname = (EditText) findViewById(R.id.lastNameText);
+      //  f=new Firebase("https://treasurehunt-457a1.firebaseio.com/");
+
+
+        logout = (Button)findViewById(R.id.logout);
+        logout.setOnClickListener(this);
+        FirebaseUser currentuser=firebaseAuth.getCurrentUser();//know current user logged in as we have to log him out
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == logout) {
+
+        firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this,MainActivity.class));
+        }
+
+        if(v==getdetails)
+        {
+
+             //will implement later
+
+        }
+
+    }
+}
